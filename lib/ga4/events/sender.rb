596cfcb949864dd4ade8b111a0f766927ca28b0c
loqimean <vanuha277@gmail.com>
2024-03-16 17:30:38 +0200

    initial commit

---
+++ lib/ga4/events/sender.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "sender/version"
+
+module Ga4
+  module Events
+    module Sender
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ga4/events/sender/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module GA4
+  module Events
+    module Sender
+      VERSION = "0.1.0"
+    end
+  end
+end
